447fc872cd6d400284e7ab1118e7169a8522b9b7
Stefnotch <brandmairstefan@gmail.com>
2025-10-25 20:10:34 +0200

    Move files to standard structure

---
+++ src/tutorial.js
@@ -0,0 +1,73 @@
+class Tutorial {
+
+    async start() {
+        await this.initializeWebGPU();
+        await this.initializeBuffers();
+        await this.initializeTextures();
+        await this.initializeLayouts();
+        await this.initializePipelines();
+        await this.initializeBindGroups();
+        await this.initializeAttachments();
+        await this.initializeGUI();
+        await this.render();
+    }
+
+    /**
+     * @param {any} gui 
+     * @param {HTMLCanvasElement} canvas 
+     */
+    constructor(gui, canvas) {
+        this.gui = gui;
+        this.canvas = canvas;
+    }
+
+    // From here on, it is up to you to implement the tutorial
+    // ----------------------------------------------------------------------------------------------------
+
+    async initializeWebGPU() {
+
+    }
+
+    async initializeBuffers() {
+
+    }
+
+    async initializeTextures() {
+        
+    }
+
+    async initializeLayouts() {
+        
+    }
+
+    async initializePipelines() {
+
+    }
+
+    async initializeBindGroups() {
+
+    }
+
+    async initializeAttachments() {
+        
+    }
+
+    async initializeGUI() {
+
+    }
+
+    /**
+     * @template {Float32Array | Uint32Array | Int32Array} T
+     * @param {GPUBuffer} gpuBuffer
+     * @param {T} outputArray
+     * @returns {Promise<T>}
+     */
+    async readBuffer(gpuBuffer, outputArray) {
+        
+    }
+
+    async render() {
+
+    }
+
+}
